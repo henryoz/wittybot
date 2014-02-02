b08da4bf6ebb688cba84c71e76583063cdb5b7ad
henryoz <henryoz@users.noreply.github.com>
2014-02-02 00:30:35 +0000

    Oops - removed API keys

--- witty.js
+++ witty.js
@@ -11,20 +11,16 @@ app.listen(3000);
 
 // twitter app info
 var T = new Twit({
-  consumer_key:         'DLPWrxClUBkrkIehxtuew', 
-  consumer_secret:      'G81mzNZf3nq13ONV6lMgMeFmJZWoSnFOVcVb7ElYzv8',
-  access_token:         '2276606983-M0kJ85NkVaqnrReZegimAezLWkLzUsLfSaYWsDa',
-  access_token_secret:  'uaKCI54C3I2xo4YI4hZZsL7hHskjsNbLkIaXRvSpkgTF0'
+  consumer_key:         'Null', 
+  consumer_secret:      'Null',
+  access_token:         'Null',
+  access_token_secret:  'Null'
 });
 
 var statement =   "";
 
 // Wordnik API info
-var getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
-                  "minCorpusCount=1000&minDictionaryCount=10&" +
-                  "excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&" +
-                  "hasDictionaryDef=true&includePartOfSpeech=noun&limit=2&maxLength=12&" +
-                  "api_key=fbf1963c205cb0fd3c007018a4106fbdfefc34f2ac581a165";
+var getNounsURL = "Null";
 
 
 
